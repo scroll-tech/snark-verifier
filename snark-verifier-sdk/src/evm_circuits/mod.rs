377b3557ca66997a85d8df74075d6be059aaf90b
zhenfei <zhenfei.zhang@hotmail.com>
2023-03-30 11:09:24 -0400

    [feat] expand circuit unit tests with aggregation and evm verification; two layers recursion unit test

--- snark-verifier-sdk/src/evm_circuits/mod.rs
+++ snark-verifier-sdk/src/evm_circuits/mod.rs
@@ -11,11 +11,12 @@ mod super_circuit;
 #[cfg(all(test, feature = "zkevm"))]
 mod test {
     use crate::{
+        evm::{evm_verify, gen_evm_proof_shplonk, gen_evm_verifier_shplonk},
         gen_pk,
-        halo2::{gen_snark_shplonk, verify_snark_shplonk},
+        halo2::{aggregation::AggregationCircuit, gen_snark_shplonk, verify_snark_shplonk},
         CircuitExt,
     };
-    use ark_std::test_rng;
+    use ark_std::{end_timer, start_timer, test_rng};
     use bus_mapping::{circuit_input_builder::CircuitsParams, mock::BlockData};
     use eth_types::{address, bytecode, geth_types::GethData, U256};
     use ethers_signers::{LocalWallet, Signer};
@@ -38,6 +39,7 @@ mod test {
     };
 
     const TEST_CURRENT_K: u32 = 19;
+    const TEST_AGG_K: u32 = 24;
     const TEST_MAX_CALLDATA: usize = 3200;
     const TEST_MAX_INNER_BLOCKS: usize = 1;
     const TEST_MAX_TXS: usize = 1;
@@ -161,15 +163,179 @@ mod test {
     }
 
     fn verify_circuit<C: CircuitExt<Fr>>(circuit: C) -> bool {
-        env::set_var("VERIFY_CONFIG", "./configs/verify_circuit.config");
+        std::env::set_var("VERIFY_CONFIG", "./configs/example_evm_accumulator.config");
 
         let mut rng = test_rng();
         let params = gen_srs(TEST_CURRENT_K);
+        let params_outer = gen_srs(TEST_AGG_K);
+        log::info!("finished parameter generation");
 
         let pk = gen_pk(&params, &circuit, None);
         let vk = pk.get_vk();
+        log::info!("finished key extraction");
 
         let snark = gen_snark_shplonk(&params, &pk, circuit, &mut rng, None::<String>);
-        verify_snark_shplonk::<C>(&params, snark, vk)
+        log::info!("finished snark generation");
+
+        if !verify_snark_shplonk::<C>(&params, snark.clone(), vk) {
+            log::error!("snark verification failed");
+            return false;
+        }
+        log::info!("snark verification succeeded");
+
+        let agg_circuit = AggregationCircuit::new(&params_outer, [snark], &mut rng);
+        let pk_outer = gen_pk(&params_outer, &agg_circuit, None);
+
+        log::info!("finished aggregation circuit generation");
+
+        let instances = agg_circuit.instances();
+        let proof = gen_evm_proof_shplonk(
+            &params_outer,
+            &pk_outer,
+            agg_circuit.clone(),
+            instances.clone(),
+            &mut rng,
+        );
+
+        log::info!("finished aggregation proof generation");
+
+        let deployment_code = gen_evm_verifier_shplonk::<AggregationCircuit>(
+            &params_outer,
+            pk_outer.get_vk(),
+            agg_circuit.num_instance(),
+            None,
+        );
+        log::info!("finished byte code generation");
+        evm_verify(deployment_code, instances, proof);
+        log::info!("EVM verification succeeded");
+        true
+    }
+
+    #[test]
+    fn super_circuit_two_layer_recursion() {
+        let mut rng = test_rng();
+        let params = gen_srs(TEST_CURRENT_K);
+        let params_layer_1 = gen_srs(25);
+        let params_layer_2 = gen_srs(25);
+        log::info!("finished parameter generation");
+
+        //
+        // load circuit and generate first layer proof
+        //
+        let super_circuit_snark = {
+            let circuit = build_circuit::<
+                SuperCircuit<
+                    Fr,
+                    TEST_MAX_TXS,
+                    TEST_MAX_CALLDATA,
+                    TEST_MAX_INNER_BLOCKS,
+                    TEST_MOCK_RANDOMNESS,
+                >,
+            >();
+            log::info!("finished super circuit generation");
+
+            let pk = gen_pk(&params, &circuit, None);
+            let vk = pk.get_vk();
+            log::info!("finished key extraction");
+            log::info!("domain size {}", vk.get_domain().k());
+            let super_circuit_timer = start_timer!(|| "super circuit snark gen");
+            let super_circuit_snark =
+                gen_snark_shplonk(&params, &pk, circuit, &mut rng, None::<String>);
+            end_timer!(super_circuit_timer);
+
+            let super_circuit_timer = start_timer!(|| "super circuit rust verify (optional)");
+            if !verify_snark_shplonk::<
+                SuperCircuit<
+                    Fr,
+                    TEST_MAX_TXS,
+                    TEST_MAX_CALLDATA,
+                    TEST_MAX_INNER_BLOCKS,
+                    TEST_MOCK_RANDOMNESS,
+                >,
+            >(&params, super_circuit_snark.clone(), vk)
+            {
+                log::error!("super circuit snark verification failed");
+                return;
+            }
+            log::info!("super circuit snark verification succeeded");
+            end_timer!(super_circuit_timer);
+            super_circuit_snark
+        };
+        //
+        // build first layer recursion proof
+        //
+        std::env::set_var("VERIFY_CONFIG", "./configs/two_layer_recursion_first_layer.config");
+        let layer_1_snark = {
+            let agg_circuit =
+                AggregationCircuit::new(&params_layer_1, [super_circuit_snark], &mut rng);
+            let pk = gen_pk(&params_layer_1, &agg_circuit, None);
+            let vk = pk.get_vk();
+            log::info!("domain size {}", vk.get_domain().k());
+
+            log::info!("finished layer 1 aggregation circuit generation");
+            let layer_1_circuit_timer = start_timer!(|| "layer 1 circuit snark gen");
+            let layer_1_snark = gen_snark_shplonk(
+                &params_layer_1,
+                &pk,
+                agg_circuit.clone(),
+                &mut rng,
+                None::<String>,
+            );
+            end_timer!(layer_1_circuit_timer);
+
+            let layer_1_circuit_timer = start_timer!(|| "layer 1 circuit rust verify (optional)");
+            if !verify_snark_shplonk::<
+                SuperCircuit<
+                    Fr,
+                    TEST_MAX_TXS,
+                    TEST_MAX_CALLDATA,
+                    TEST_MAX_INNER_BLOCKS,
+                    TEST_MOCK_RANDOMNESS,
+                >,
+            >(&params_layer_1, layer_1_snark.clone(), vk)
+            {
+                log::error!("layer 1 snark verification failed");
+                return;
+            }
+            log::info!("layer 1 snark verification succeeded");
+            end_timer!(layer_1_circuit_timer);
+            layer_1_snark
+        };
+        //
+        // verify layer 1 snark with evm
+        //
+        {
+            std::env::set_var("VERIFY_CONFIG", "./configs/two_layer_recursion_second_layer.config");
+            
+            let agg_circuit = AggregationCircuit::new(&params_layer_2, [layer_1_snark], &mut rng);
+            let pk_outer = gen_pk(&params_layer_2, &agg_circuit, None);
+            log::info!("finished layer 2 aggregation circuit generation");
+            log::info!("domain size {}", pk_outer.get_vk().get_domain().k());
+
+            let instances = agg_circuit.instances();
+            let layer_2_circuit_timer = start_timer!(|| "layer 2 circuit snark gen");
+            let proof = gen_evm_proof_shplonk(
+                &params_layer_2,
+                &pk_outer,
+                agg_circuit.clone(),
+                instances.clone(),
+                &mut rng,
+            );
+            end_timer!(layer_2_circuit_timer);
+            log::info!("finished layer 2 aggregation proof generation");
+
+            let layer_2_circuit_timer = start_timer!(|| "layer 2 circuit evm verify");
+            let deployment_code = gen_evm_verifier_shplonk::<AggregationCircuit>(
+                &params_layer_2,
+                pk_outer.get_vk(),
+                agg_circuit.num_instance(),
+                None,
+            );
+            log::info!("finished byte code generation");
+
+            evm_verify(deployment_code, instances, proof);
+            end_timer!(layer_2_circuit_timer);
+            log::info!("EVM verification succeeded");
+        }
     }
 }
