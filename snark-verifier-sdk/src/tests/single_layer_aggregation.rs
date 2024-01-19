ebcfd9487c4310c2026cf62c8a2065aa56c0afd9
Steven Gu <asongala@163.com>
2024-01-19 09:43:10 +0800

    U

--- snark-verifier-sdk/src/tests/single_layer_aggregation.rs
+++ snark-verifier-sdk/src/tests/single_layer_aggregation.rs
@@ -21,8 +21,8 @@ fn test_shplonk_then_sphplonk_with_evm_verification() {
     let k_agg = 24;
 
     let mut rng = test_rng();
-    let params_outer = gen_srs(k_agg);
     let params_inner = {
+        let params_outer = gen_srs(k_agg);
         let mut params = params_outer.clone();
         params.downsize(k);
         params
@@ -38,51 +38,20 @@ fn test_shplonk_then_sphplonk_with_evm_verification() {
         &mut rng,
         Some(Path::new("data/inner_1.snark")),
     );
-    println!("finished snark generation for circuit 1");
-
-    // Another Proof for circuit 1
-    let circuit_2 = TestCircuit1::rand(&mut rng);
-    let pk_inner_2 = gen_pk(&params_inner, &circuit_2, Some(Path::new("data/inner_2.pkey")));
-    let snarks_2 = gen_snark_shplonk(
-        &params_inner,
-        &pk_inner_2,
-        circuit_2.clone(),
-        &mut rng,
-        Some(Path::new("data/inner_2.snark")),
-    );
-    println!("finished snark generation for circuit 1");
-
-    // Proof for circuit 2
-    let circuit_3 = TestCircuit2::rand(&mut rng);
-    let pk_inner_3 = gen_pk(&params_inner, &circuit_3, Some(Path::new("data/inner_3.pkey")));
-    let snarks_3 = gen_snark_shplonk(
-        &params_inner,
-        &pk_inner_3,
-        circuit_3.clone(),
-        &mut rng,
-        Some(Path::new("data/inner_3.snark")),
-    );
-    println!("finished snark generation for circuit 1");
-
-    // aggregation circuit
-    let snarks = vec![snarks_1, snarks_2, snarks_3];
-    let agg_circuit = AggregationCircuit::new(&params_outer, snarks, &mut rng);
-    let pk_outer = gen_pk(&params_outer, &agg_circuit, Some(Path::new("data/outer.pkey")));
-    println!("finished outer pk generation");
-    let instances = agg_circuit.instances();
+    let instances = circuit_1.instances();
     let proof = gen_evm_proof_shplonk(
-        &params_outer,
-        &pk_outer,
-        agg_circuit.clone(),
+        &params_inner,
+        &pk_inner_1,
+        circuit_1.clone(),
         instances.clone(),
         &mut rng,
     );
     println!("finished aggregation generation");
 
-    let deployment_code = gen_evm_verifier::<AggregationCircuit, Kzg<Bn256, Bdfg21>>(
-        &params_outer,
-        pk_outer.get_vk(),
-        agg_circuit.num_instance(),
+    let deployment_code = gen_evm_verifier::<TestCircuit1, Kzg<Bn256, Bdfg21>>(
+        &params_inner,
+        pk_inner_1.get_vk(),
+        circuit_1.num_instance(),
         Some(Path::new("data/single_layer_recur.sol")),
     );
 
